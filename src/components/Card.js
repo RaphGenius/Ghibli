d008221b3115c69ca7b8a0acad379c81463a4e2d
Raphael <guet.raphael@hotmail.com>
2022-09-09 18:47:25 +0200

    Fin du style du back des cartes - ajout des boutons

--- src/components/Card.js
+++ src/components/Card.js
@@ -15,35 +15,23 @@ export default function Card({ movie }) {
         </div>
       </div>
       <div className="card back">
-        <div className="description">
-          <p>{movie.description} </p>
+        <div className="container container_back">
+          <div className="movie_title">
+            <p className="__title">{movie.title} </p>
+            <p className="__title">{movie.original_title} </p>
+          </div>
+          <div className="description">
+            <p>Sortie en {movie.release_date} </p>
+            <p>
+              Résumé : <br /> {movie.description}{" "}
+            </p>
+          </div>
+          <div className="btn-part">
+            <button className="btn">En savoir plus</button>
+            <button className="btn">Ajouter à ma liste</button>
+          </div>
         </div>
       </div>
     </div>
   );
 }
-
-/*     <div className="Fullcard">
-      <div className="face front">
-        <div className="container-img">
-          <img
-            className="__img"
-            src={movie.image}
-            key={movie.id}
-            alt={"Affiche de" + movie.title}
-          ></img>
-        </div>
-        <div className="container-title">
-          <p className="__title">{movie.title} </p>
-        </div>
-      </div>
-    </div> */
-
-//CARTE QUI RETOURNE QUI MARCHE
-/*     <div className="container">
-    <div className="card front">
-      Front
-      <h1>Salut</h1>
-    </div>
-    <div className="card back">Back</div>
-  </div> */
